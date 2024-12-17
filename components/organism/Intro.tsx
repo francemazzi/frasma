c582ea97486a92dac16854602c1a13d6161c1123
FraSma <francemazzi@gmail.com>
2024-12-17 13:11:47 +0100

    feat: optimiaze title

--- components/organism/Intro.tsx
+++ components/organism/Intro.tsx
@@ -32,7 +32,7 @@ export default function Intro() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.2 }}
       >
-        Welcome to my world as an agricultural startup enthusiast
+        Welcome to my world as a startup enthusiast
       </motion.h1>
       <motion.div
         className="max-w-3xl mx-auto"
