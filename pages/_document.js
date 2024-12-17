a9f62388ca5829eb0a875f0ac60edf0de290ee7a
FraSma <francemazzi@gmail.com>
2024-12-17 13:27:33 +0100

    feat: add image

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,11 @@ class MyDocument extends Document {
           {/* firebase */}
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="..."
+          />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,200;1,500;1,600&display=swap"
             rel="stylesheet"
