037b1cf2471a78de788076681a8c771601a1e38e
FraSma <francemazzi@gmail.com>
2026-03-31 16:10:26 +0200

    feat: integrate Google Analytics with gtag.js for improved tracking and analytics capabilities

--- pages/_document.js
+++ pages/_document.js
@@ -16,6 +16,20 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             }}
           />
           {/* End Google Tag Manager */}
+          {/* Google tag (gtag.js) */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-C33CLQ0SZ4"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-C33CLQ0SZ4');`,
+            }}
+          />
+          {/* End Google tag (gtag.js) */}
           {/* firebase */}
           <meta
             name="viewport"
